c69dd73d40951839988304c2e7ea3fdfb68d06f3
d3adp007 <nikhil.nagpal11396@gmail.com>
2019-02-25 22:35:33 +0530

    Add files via upload

---
+++ src/main/java/com/interview/testing/urlshorter/module/UrlshorterApplication.java
@@ -0,0 +1,12 @@
+package com.interview.testing.urlshorter.module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlshorterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlshorterApplication.class, args);
+	}
+}
